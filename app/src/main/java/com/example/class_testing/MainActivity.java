58165ba0f72343d19246073ff60342e40cdc31b1
b21emmbj <b21emmbj@student.his.se>
2022-04-08 14:54:16 +0200

    getters and setters working, tested w logcat

--- app/src/main/java/com/example/class_testing/MainActivity.java
+++ app/src/main/java/com/example/class_testing/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.class_testing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Berg a = new Berg("Billingen", "Skövde", 304);
+        Log.d("==HERP==", a.getName());
     }
+
 }
