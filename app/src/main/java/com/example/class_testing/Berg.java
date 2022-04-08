4dcd0735a717624ab812346e06cba179cb45031e
b21emmbj <b21emmbj@student.his.se>
2022-04-08 14:20:34 +0200

    Seed code

---
+++ app/src/main/java/com/example/class_testing/Berg.java
@@ -0,0 +1,15 @@
+package com.example.class_testing;
+
+public class Berg {
+
+    private String name;
+    private  String location;
+    private int height_n;
+
+
+    public Berg(String name, String location, int height_n) {
+        this.name = name;
+        this.location = location;
+        this.height_n = height_n;
+    }
+}
